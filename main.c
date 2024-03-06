8c009baaf6fcf3704b3283d933d875b8d29a7cf3
imRoei <roeigoldi9@gmail.com>
2024-03-06 13:18:38 +0200

    first upload - including working on key generation

---
+++ main.c
@@ -0,0 +1,9 @@
+
+
+int main()
+{
+    // Your main encryption process
+    // Initialize your block and keys
+    // Call serpentEncrypt with the block and keys
+    return 0;
+}
---
+++ serpent.c
@@ -0,0 +1,95 @@
+// headers that have been used in the project
+#include <stdio.h>  // for reading from stdin and writing to stdout
+#include <stdlib.h> // for managing memory allocation
+#include <string.h> //
+#include <stdint.h> //
+#include <string.h>
+#include <limits.h> // Include for CHAR_BIT
+#include "Sbox.h"
+
+// defines
+#define ROTL(x, y) (((x) << (y)) | ((x) >> (32 - (y)))) // ROTL Bits Macro
+#define FRAC 0x9e3779b9                                 //  fractional part of the golden ratio(make the cipher safer)
+#define MAX_KEY_LENGTH 32                               // maximum key length
+
+/*
+ key padding is the first part of the cipher and it is done to make sure that the key
+ is the correct size match the functions
+ return the length of the padded key, always 32 bytes
+*/
+int Key_Padding(const unsigned char *key, unsigned int keyLength, unsigned char *paddedKey)
+{
+    // Initialize the padded key buffer
+    memset(paddedKey, 0, MAX_KEY_LENGTH);
+    // In case the original key length is less than 32 byte
+    if (keyLength < MAX_KEY_LENGTH)
+    {
+        // copy the original key to the padded key
+        memcpy(paddedKey, key, keyLength);
+        // add the padding byte after the original key
+        // 0x80 in hexadecimal represents 1000 0000 in binary, which corresponds to this pattern
+        // of the padding
+        paddedKey[keyLength] = 0x01;
+    }
+    else
+    {
+        // copy the original key to the padded key
+        memcpy(paddedKey, key, MAX_KEY_LENGTH);
+    }
+    return MAX_KEY_LENGTH; // always 32
+}
+
+// key schedule is the second part of the cipher and it is done in order to
+//  make sure the encryption is more efficient instead of calculating
+// the round keys (will be done in all the 32 round and would cost alot more).
+void Get_Prekeys(uint32_t *key, uint32_t *prekeys)
+{
+    // fields to make the key safer
+    uint32_t keysplit[8] = {0};
+    uint32_t interkey[140] = {0};
+
+    // According to the Serpent specification, the first 8 prekeys are directly copied from the padded key
+    for (int i = 0; i < 8; ++i)
+    {
+        prekeys[i] = key[i];
+    }
+
+    for (int i = 8; i < 140; ++i)
+    {
+        interkey[i] = rotl((interkey[i - 8] ^ interkey[i - 5] ^ interkey[i - 3] ^ interkey[i - 1] ^ FRAC ^ (i - 8)), 11);
+    }
+}
+
+void Key_Scedule(uint32_t subkeysHat[33][4],
+                 const unsigned char *key,
+                 unsigned char *output,
+                 uint32_t nBytes)
+{
+    // 33 subkeys * 32bits * 4 blocks
+    uint32_t subkeys[33][4] = {0};
+    uint32_t prekey[140] = {0};
+
+    // memory precheck
+    // the program will not work if the pointer to the ciphered text
+    // is NULL
+    if (output == NULL)
+    {
+        fprintf(stderr, "Given output char pointer not initialized/allocated.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    unsigned char paddedKey[MAX_KEY_LENGTH] = {0}; // Allocate on the stack to avoid manual memory management
+
+    // check if the key padding is needed for this length of key
+    if (nBytes != 32)
+    {
+        Key_Padding(key, nBytes, paddedKey);
+    }
+    else
+    {
+        memcpy(paddedKey, key, MAX_KEY_LENGTH);
+    }
+    // make a casting to a number in order to make mathematical operations
+    uint32_t *paddedKeyInt = (uint32_t *)paddedKey;
+    Get_Prekeys(paddedKeyInt, prekey);
+}
